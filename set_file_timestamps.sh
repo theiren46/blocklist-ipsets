2629844788cdf644a025e859c91930dc1acbb44f
Costa Tsaousis <costa@tsaousis.gr>
2015-07-18 11:27:38 +0300

    Sat Jul 18 08:27:38 UTC 2015 update

--- set_file_timestamps.sh
+++ set_file_timestamps.sh
@@ -15,7 +15,7 @@
 [ -f 'blocklist_de_bots.ipset' ] && touch --date=@1437207270 'blocklist_de_bots.ipset'
 [ -f 'blocklist_de_bruteforce.ipset' ] && touch --date=@1437207273 'blocklist_de_bruteforce.ipset'
 [ -f 'blocklist_de_ftp.ipset' ] && touch --date=@1437207248 'blocklist_de_ftp.ipset'
-[ -f 'blocklist_de_imap.ipset' ] && touch --date=@1437205334 'blocklist_de_imap.ipset'
+[ -f 'blocklist_de_imap.ipset' ] && touch --date=@1437207247 'blocklist_de_imap.ipset'
 [ -f 'blocklist_de_mail.ipset' ] && touch --date=@1437206165 'blocklist_de_mail.ipset'
 [ -f 'blocklist_de_sip.ipset' ] && touch --date=@1437204613 'blocklist_de_sip.ipset'
 [ -f 'blocklist_de_ssh.ipset' ] && touch --date=@1437207006 'blocklist_de_ssh.ipset'
