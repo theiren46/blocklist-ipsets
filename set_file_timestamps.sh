5ecd6f2185ae4168f03f001c465ec273c6030567
Costa Tsaousis <costa@tsaousis.gr>
2015-07-22 03:27:50 +0300

    Wed Jul 22 00:27:50 UTC 2015 update

--- set_file_timestamps.sh
+++ set_file_timestamps.sh
@@ -548,7 +548,7 @@
 [ -f 'ipdeny_country/id_country_za.netset' ] && touch --date=@1436443632 'ipdeny_country/id_country_za.netset'
 [ -f 'ipdeny_country/id_country_zm.netset' ] && touch --date=@1435493234 'ipdeny_country/id_country_zm.netset'
 [ -f 'ipdeny_country/id_country_zw.netset' ] && touch --date=@1436616437 'ipdeny_country/id_country_zw.netset'
-[ -f 'iw_spamlist.ipset' ] && touch --date=@1437520804 'iw_spamlist.ipset'
+[ -f 'iw_spamlist.ipset' ] && touch --date=@1437524404 'iw_spamlist.ipset'
 [ -f 'iw_wormlist.ipset' ] && touch --date=@1437492005 'iw_wormlist.ipset'
 [ -f 'lashback_ubl.ipset' ] && touch --date=@1437519080 'lashback_ubl.ipset'
 [ -f 'malc0de.ipset' ] && touch --date=@1437484622 'malc0de.ipset'
