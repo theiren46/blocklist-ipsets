ebd54d8e1d119e3c9f8bda1cb4e078c1832c1981
Costa Tsaousis <costa@tsaousis.gr>
2015-07-28 08:00:36 +0300

    Tue Jul 28 05:00:36 UTC 2015 update

--- set_file_timestamps.sh
+++ set_file_timestamps.sh
@@ -287,7 +287,7 @@
 [ -f 'dragon_sshpauth.netset' ] && touch --date=@1438056244 'dragon_sshpauth.netset'
 [ -f 'dragon_vncprobe.netset' ] && touch --date=@1438056242 'dragon_vncprobe.netset'
 [ -f 'dshield.netset' ] && touch --date=@1438058460 'dshield.netset'
-[ -f 'dshield_1d.netset' ] && touch --date=@1438058460 'dshield_1d.netset'
+[ -f 'dshield_1d.netset' ] && touch --date=@1438059205 'dshield_1d.netset'
 [ -f 'dshield_7d.netset' ] && touch --date=@1438058460 'dshield_7d.netset'
 [ -f 'et_block.netset' ] && touch --date=@1437971402 'et_block.netset'
 [ -f 'et_botcc.ipset' ] && touch --date=@1437971401 'et_botcc.ipset'
