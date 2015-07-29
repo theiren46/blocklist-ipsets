60f9674e0e1c9ef0ca57ff60165a4f32e11fdbfa
Costa Tsaousis <costa@tsaousis.gr>
2015-07-29 09:27:36 +0300

    Wed Jul 29 06:27:36 UTC 2015 update

--- set_file_timestamps.sh
+++ set_file_timestamps.sh
@@ -2,7 +2,7 @@
 [ ! "$1" = "YES_I_AM_SURE_DO_IT_PLEASE" ] && echo "READ ME NOW" && exit 1
 [ -f 'alienvault_reputation.ipset' ] && touch --date=@1437854422 'alienvault_reputation.ipset'
 [ -f 'geolite2_country/anonymous.netset' ] && touch --date=@1436356244 'geolite2_country/anonymous.netset'
-[ -f 'bi_any_2_1d.ipset' ] && touch --date=@1438149254 'bi_any_2_1d.ipset'
+[ -f 'bi_any_2_1d.ipset' ] && touch --date=@1438151224 'bi_any_2_1d.ipset'
 [ -f 'bi_any_2_30d.ipset' ] && touch --date=@1438093103 'bi_any_2_30d.ipset'
 [ -f 'bi_any_2_7d.ipset' ] && touch --date=@1438138450 'bi_any_2_7d.ipset'
 [ -f 'bi_bruteforce_2_30d.ipset' ] && touch --date=@1435970707 'bi_bruteforce_2_30d.ipset'
